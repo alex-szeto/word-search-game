41f23ae44c9c867eb588c2ffe225a5c47280573d
github username <github email address>
2020-02-28 11:58:49 -0500

    added relationships

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,2 +1,11 @@
 class GameSessionsController < ApplicationController
+    def show
+        gameSession = GameSession.find(params[:id])
+        render json: gameSession
+    end
+    
+    def index
+        gameSessions = GameSession.all
+        render json: gameSessions
+    end
 end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,11 @@
 class WordsController < ApplicationController
+    def show
+        word = Word.find(params[:id])
+        render json: word
+    end
+    
+    def index
+        words = Word.all
+        render json: words
+    end
 end
