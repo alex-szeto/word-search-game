9f6d8386a789bd0c629d4c66b9802335563be105
github username <github email address>
2020-03-02 09:52:44 -0500

    added css

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentload", runGame)
-
-
-const runGame = e => {
-
-}
+const body = document.getElementById("body")
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log("loaded")
+    const div = document.createElement("div")
+    div.innerHTML = "<h1>Hello</h1>"
+    // body.append(div)
+})
